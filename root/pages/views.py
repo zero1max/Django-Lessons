21587ca72f30ea921f043c25b8ef38f3be22da1c
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-12-16 20:20:09 +0500

    Update commit

--- root/pages/views.py
+++ root/pages/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import Post
 
 # Create your views here.
 def home(request):
     return render(request, template_name='page/index.html')
 
-def users(request):
-    return render(request, template_name='page/user.html')
+def posts(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, template_name='page/post.html', context=context)
