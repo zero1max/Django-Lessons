da9177e23aa1c47519939f4994c3584b925172e0
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-12-18 14:24:37 +0500

    Update Commit

--- root/pages/views.py
+++ root/pages/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 
-# Create your views here.
+
 def post_list(request):
     posts = Post.published.all()
-    return render(request, "blog/post/list.html", {'posts': posts})
+    return render(request, "blog/post/list.html", context={"posts": posts})
 
-def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Post not found")
-    return render(request, "blog/post/detail.html", {'post': post})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
+
+    return render(
+        request,
+        "blog/post/detail.html",
+        context={"post": post}
+    )
