7f836eb795147f02fd9868ed95e1bde875e23f89
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-12-18 20:49:28 +0500

    Update Commit

--- root/pages/views.py
+++ root/pages/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
+from django.core.paginator import Paginator
+
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 1)# 1 bu har bir page da nechta blog bolish kerakligi!
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     return render(request, "blog/post/list.html", context={"posts": posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
