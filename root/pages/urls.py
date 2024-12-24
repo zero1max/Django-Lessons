0d1bf25b2fcef228ceefa4a09cc6f9ecdbd37c81
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-12-24 10:36:43 +0500

    Update Commit

--- root/pages/urls.py
+++ root/pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import post_detail,post_list
+from .views import post_detail, PostListView
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('',post_list,name="post_list"),
-    path("<int:year>/<int:month>/<int:day>/<slug:post>/",post_detail,name="post_detail")
+    # Post views
+    # path('', views.post_list, name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',post_detail,name='post_detail'),
 ]
